fef54697c1eac99be85ac161e54bde286f171149
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-21 09:59:02 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/* The Alphabet Game */
+
+/**
+ * main - A program that prints the alphabets in lowercase followed by a new line
+ * Return: Always 0 (Success)
+ */
+char main (void)
+{
+	for (char x = 'a'; x <= 'z'; x++)
+	       putchar(x);
+		putchar(\n);
+		return (0);
+}		
