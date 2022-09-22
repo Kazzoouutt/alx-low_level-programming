d2e64769606b6ac5e3ec4b2f58fb7567e165129a
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-21 21:06:19 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Prints letters in lowercase then uppercase
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97, j = 65;
+
+	while ( i<= 122)
+	{
+		putchar(i);
+		i++;
+
+		while (j <= 90)
+		{
+			putchar(j);
+			j++;
+		}
+	}
+	putchar(10);
+	return (0);
+}
