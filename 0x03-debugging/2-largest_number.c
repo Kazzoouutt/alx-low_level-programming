12d74eb21243ac10521e02e7f2b8459bc1a5a305
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-22 16:23:53 -0700

    finaal task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,11 +18,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if ((b > a && a >= c) || (b > c && c >= a))
+	else if ((b >= a && a >= c) || (b >= c && c >= a))
 	{
 		largest = b;
 	}
-	else if ((c > a && a >= b) || (c > b && b >= a))
+	else if ((c >= a && a >= b) || (c >= b && b >= a))
 	{
 		largest = c;
 	}
