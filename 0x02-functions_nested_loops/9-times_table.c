6dd61d73d24e39cea8c1f8d4c4d450a9343b5638
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-22 06:35:02 -0700

    task 9 done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,17 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
+		_putchar('0');
+
 		for (y = 0; y <= 9; y++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			z = x * y;
 
 			if (z <= 9)
-			{
-				_putchar(',');
 				_putchar(' ');
-			}
-			
 			else
 				_putchar((z / 10) + '0');
 
