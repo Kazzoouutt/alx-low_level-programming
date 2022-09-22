6b191d4b59c92e20585194fc37edad87fa32a6fb
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-22 16:19:28 -0700

    task 2 final

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,16 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	scanf("%d %d %d", &a, &b, &c);
-	printf("a = %d\tb = %d\tc = %d\n", a, b, c);
 
-	if (a > b && b > c)
+	if ((a >= b && b >= c) || (a >= c && c >= b))
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if ((b > a && a >= c) || (b > c && c >= a))
 	{
 		largest = b;
 	}
-	else
+	else if ((c > a && a >= b) || (c > b && b >= a))
 	{
 		largest = c;
 	}
