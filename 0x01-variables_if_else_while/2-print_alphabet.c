b79a8e8a28bcb89df325793ce27e8e46f74c69fa
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-21 20:46:21 -0700

    task 2ac

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,18 @@
 /* The Alphabet Game */
 
 /**
- * main - A program that prints the alphabets in lowercase followed by a new line
+ * main - Prints alphabets in lowercase followed by a new line
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	int i = 97; 
-	
-	while(i <= 122)
+	int i = 97;
+
+	while (i <= 123)
 	{
 		putchar(i);
 		i++;
 	}
-	
 	putchar(10);
 	return (0);
-}		
+}
