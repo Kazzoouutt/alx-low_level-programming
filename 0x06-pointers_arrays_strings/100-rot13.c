9b4d1bf812b048ffc48d74325f22ac8d45e40c97
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-23 02:38:55 -0700

    task 100 done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,19 +9,20 @@
 
 char *rot13(char *s)
 {
-       int i = 0;
+	int i = 0;
 
-       while (s[i] != '\0')
-       {
-	       while (((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M')))
-	       {
-		       s[i] += 13;
-		       i++;
-	       }
-	       if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-		      s[i] -= 13;
-	       i++;
-       }
+	while (s[i] != '\0')
+	{
+		while (((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M')))
+		{
+			s[i] += 13;
+			i++;
+		}
 
-       return (s);
+		if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			s[i] -= 13;
+		i++;
+	}
+
+	return (s);
 }
