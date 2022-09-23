decf73434bc310e938debef1ce8fc92101122adb
Kazzoouutt <111151488+Kazzoouutt@users.noreply.github.com>
2022-09-22 22:50:51 -0700

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: strings to print
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int j, i =0;
+
+	while (*(str + 1))
+		i++;
+	
+	j = i / 2;
+	if (i % 2)
+		j += 1;
+	
+	while (j < i)
+	{
+		_putchar(*(str + j));
+		j++;
+	}
+	_putchar('\n')
+}
